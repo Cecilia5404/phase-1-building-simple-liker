af83ba22c34ccda7f2f9bc954395b432a5971e39
Cecil-Anyega <ceciliaanyega@gmail.com>
2022-06-17 11:47:11 +0300

    commit tree 6666e2e2944064f875903a3e600be1ef2e272d1f

--- main.js
+++ main.js
@@ -3,6 +3,38 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const unlikedHeart = document.querySelectorAll('span')
+
+unlikedHeart.forEach(element => {
+
+  element.addEventListener('click', ()=>{
+
+    likedHeart()
+    mimicServerCall().catch(function(error){
+
+      document.getElementById('modal').className = "visible"
+      likedHeart()
+
+      setTimeout(function(){ 
+        document.getElementById('modal').className = "hidden"
+      }, 3000);
+
+    })
+
+  });
+
+  function likedHeart(){
+    if (element.className === "like-glyph"){
+      element.innerHTML = FULL_HEART
+      element.className = "like-glyph activated-heart"
+
+    } else if (element.className === "like-glyph activated-heart") {
+      element.innerHTML = EMPTY_HEART
+      element.className = "like-glyph"
+    }
+}
+
+})
 
 
 
